346e2c4667dafb8a103b7079f5ad52fd95bda9b6
Brandt Davis <brandtd77@gmail.com>
2025-02-15 13:14:33 -0700

    Implement conversion from distance unit per hour to minutes per distance unit

--- server/race_multi_tool/app.py
+++ server/race_multi_tool/app.py
@@ -4,61 +4,45 @@ import math
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from .utils import utils
+from .utils.utils import (
+    DISTANCES_PER_HOUR,
+    MINUTES_PER_DISTANCE,
+    convert_dist_per_hour_to_min_per_dist,
+    convert_mins_per_dist_to_dist_per_hour,
+    )
 
 app = Flask(__name__)
 cors = CORS(app, origin="*")
 
-MILES_TO_KM = 1.60934
-
-@app.route("/miles_per_hour_to_mins_per_km", methods=['GET'])
-def miles_per_hour_to_mins_per_km() -> jsonify:
-    """ Converts miles per hour to minutes per km"""
-    miles_per_hour = request.args.get("miles_per_hour")
-    km_per_hour = float(miles_per_hour) * MILES_TO_KM
-
-    mins_per_km = 60.0 / float(km_per_hour)
-
-    # calculates fractional values as seconds
-    mins = math.floor(mins_per_km)
-    print("MINS: ", mins)
-    seconds = utils.format_seconds(int((mins_per_km % 1) * 60))
-    print("seconds: ", mins_per_km % 1 * 60)
-
-    return jsonify({"min_per_km": f"{mins}:{seconds} per km"})
-
-@app.route("/kms_per_hour_to_mins_per_km", methods=['GET'])
-def kms_per_hour_to_mins_per_km():
-    """ Converts km per hour to minutes per km"""
-    km_per_hour = request.args.get("km_per_hour")
-    mins_per_km = 60.0 / float(km_per_hour)
-
-    # calculates fractional values as seconds
-    mins = math.floor(mins_per_km)
-    seconds = utils.format_seconds(int((mins_per_km % 1) * 60))
-
-    return jsonify({"min_per_km": f"{mins}:{seconds} per km"})
-
 @app.route("/convert_pace", methods=["GET"])
 def convert_pace():
     """ Converts input pace to desired format """
-    pace = float(request.args.get("pace"))
-    input_units = float(request.args.get("input_units"))
-    output_units = float(request.args.get("output_units"))
+    # e.g., 10, 4:50
+    pace = str(request.args.get("pace"))
 
-    # error checking
+    # e.g., miles/hr
+    input_units = str(request.args.get("input_units")).lower()
 
-    # determine what math we need to do
-    # parse input and output units to deterimine:
-    # input time and distance units
-    # output time and distance units
+    # e.g., min/km
+    desired_units = str(request.args.get("output_units")).lower()
 
-    # cases:
-    # 1) min/x <-> min/y
-    # 2) x/hr <-> y/hr
-    # 3) x/hr <-> min/x
-    # 4) min/x <-> y/hr
+    # TODO: error checking
 
+    result = {}
+    # logic
+    if input_units in DISTANCES_PER_HOUR:
+        desired_pace = convert_dist_per_hour_to_min_per_dist(
+            float(pace),
+            input_units,
+            desired_units
+        )
+        result = {"result": desired_pace}
+
+    if input_units in MINUTES_PER_DISTANCE:
+        desired_pace = convert_mins_per_dist_to_dist_per_hour(int(pace), input_units, desired_units)
+        result = {"result": desired_pace}
+
+    return jsonify(result)
 
 
 if __name__ == 'main':
--- server/race_multi_tool/utils/utils.py
+++ server/race_multi_tool/utils/utils.py
@@ -1,11 +1,54 @@
 """ This module stores the main 'commonly' used or misc. functions """
 
-def convert_distance(distance: int, current_unit: str, desired_unit: str):
-    """ Converts a distance from unit to another"""
-    
+import math
 
-def format_seconds(seconds: int):
-    """ Applies proper formatting to the seconds value """
+DISTANCES_PER_HOUR = ["km/hour", "miles/hour"]
+MINUTES_PER_DISTANCE = ["min/km", "min/mile"]
+
+MILES_TO_KM = 1.60934
+
+def convert_mins_per_dist_to_dist_per_hour(distance: int, current_unit: str, desired_unit: str):
+    """ Converts pace from mins/km or mins/mile to km/hour or miles/hour """
+    return 0
+
+def convert_dist_per_hour_to_min_per_dist(distance: int, current_unit: str, desired_unit: str):
+    """ Converts pace from km/hour or miles/hour to mins/km or mins/mile """
+    mins = 0
+    seconds = 0
+
+    mins_per_dist_unit = 60.0 / float(distance)
+
+    current_distance_unit = get_distance_unit(current_unit)
+    desired_distance_unit = get_distance_unit(desired_unit)
+
+    unit_const = 1.0
+    if current_distance_unit != desired_distance_unit:
+        unit_const = MILES_TO_KM if current_distance_unit == 'km' else 1 / MILES_TO_KM
+
+    mins = get_pace_minutes(mins_per_dist_unit, unit_const)
+    seconds = get_pace_seconds(mins_per_dist_unit, unit_const)
+
+    return f"{mins}:{seconds}"
+
+def get_distance_unit(unit: str):
+    """ return the distance unit for a given pace unit """
+    pace_units = unit.split("/")
+
+    if pace_units[0] == "km" or pace_units[0] == "miles":
+        return pace_units[0]
+
+    return pace_units[1]
+
+def get_pace_minutes(mins, unit_const: float):
+    """ calculates the number of minutes per unit of distance to the nearest whole minute """
+    return math.floor(mins * unit_const)
+
+def get_pace_seconds(mins, unit_const: float):
+    """ calculates the seconds based on fractional minutes """
+    seconds = (mins * unit_const) % 1 * 60
+    seconds = math.floor(seconds + 0.5)
+
     if len(str(seconds)) == 1:
-        return f"{seconds}0"
+        seconds = f"{seconds}0"
+
     return seconds
