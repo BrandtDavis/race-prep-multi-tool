ace00ac294595755f90a2a38ecba0cd94fca92e1
Brandt Davis <brandtd77@gmail.com>
2025-03-02 10:11:09 -0700

    Add react-router, new page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import PaceConverterPage from "./pages/paceConverterPage.tsx";
+import CreateActivityPage from "./pages/createActivityPage.tsx";
 
+const Layout = () => {
+  return (
+    <div>
+      <span>This be header</span>
+      <Outlet />
+      <span>This be footer</span>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/pace-converter",
+        element: <PaceConverterPage />,
+      },
+      {
+        path: "/create-activity",
+        element: <CreateActivityPage />,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
