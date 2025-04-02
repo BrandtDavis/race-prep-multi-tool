864268b33e3490838ff715d7c81d12e5cd53a5df
Brandt Davis <brandtd77@gmail.com>
2025-04-02 13:26:11 -0600

    Add basic container to profile page

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,5 +1,11 @@
 function ProfilePage() {
-    return <div>This will be a profile page</div>;
+    return ( 
+      <div className="min pt-15 h-screen w-full flex items-center justify-center bg-gradient-to-r from-blue-200 via-blue-100 to-blue-200">
+        <div className="max-w-[500px] rounded-3xl border bg-gray-300 p-8 mx-10 shadow-xl space-y-5">
+          
+        </div>
+      </div>
+    )
   }
   
   export default ProfilePage;
