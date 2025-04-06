9c38367f8f1d8660afa444bda0426058ce903e0e
Brandt Davis <brandtd77@gmail.com>
2025-04-06 12:01:11 -0600

    Add basic useEffect hook to get user's name for nav bar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,37 @@
 import Nav from "./Nav";
+import { useEffect, useState } from "react";
+
+const getUserData = async () => {
+    const response = await fetch(
+      'http://localhost:5000/get_user_data?' + new URLSearchParams({
+            user_id: "1",
+        }),
+    { 
+        method: 'GET',
+        headers: {
+            Accept: 'application/form-data',
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+    console.log(data)
+    return data
+};
 
 function Header() {
+    const [userName, setUserName] = useState("User Unavailable");
+
+    useEffect(() => {
+        (async () => {
+            const userData = await getUserData()
+            setUserName(`${userData[1]} ${userData[2]}`)
+        })()
+    }, []);
+
     return (
         <header className="fixed top-0 z-[1] mx-auto mb-50 flex w-full max-w-7xl flex-wrap items-center justify-between border-b border-gray-100 bg-gray-800 p-[2em] font-sans font-bold uppercase text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary">
-            <h3 className="text-gray-400">USER NAME</h3>
+            <h3 className="text-gray-400">{ userName }</h3>
             <Nav />
         </header>
     )
