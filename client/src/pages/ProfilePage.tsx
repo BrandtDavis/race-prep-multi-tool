c8a79630a3a3ed586541f5ef994f54ccd3798d78
Brandt Davis <brandtd77@gmail.com>
2025-04-11 07:51:07 -0600

    Add user data to profile form

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,14 +1,52 @@
 import TextInput from "../components/inputs/TextInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const getUserData = async () => {
+  const response = await fetch(
+    'http://localhost:5000/get_user_data?' + new URLSearchParams({
+          user_id: "1",
+      }),
+  { 
+      method: 'GET',
+      headers: {
+          Accept: 'application/form-data',
+          'Content-Type': 'application/json',
+      },
+  });
+
+  const data = await response.json();
+  console.log(data)
+  return data
+};
 
 function ProfilePage() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [formattedFirstName, setFormattedFirstName] = useState("")
   
+    useEffect(() => {
+          (async () => {
+              const userData = await getUserData()
+              setFirstName(`${userData['first_name']}`)
+              setLastName(`${userData['last_name']}`)
+
+              // Appropriately format possesive name for profile form title
+              let possessiveName: string;
+              if(firstName.substring(firstName.length - 1).toLowerCase() !== 's') {
+                possessiveName = `${firstName}'s`
+              }
+              else {
+                possessiveName = `{firstName}'`
+              }
+              console.log("NAME", possessiveName)
+              setFormattedFirstName(possessiveName)
+          })()
+      }, []);
+
     return ( 
       <div className="min pt-15 h-screen w-full flex items-center justify-center bg-gradient-to-r from-blue-200 via-blue-100 to-blue-200">
         <div className="max-w-[500px] rounded-3xl border bg-gray-300 p-8 mx-10 shadow-xl space-y-5">
-          <h2 className="col-span-6 text-2xl font-bold">Your Profile</h2>
+          <h2 className="col-span-6 text-2xl font-bold">{formattedFirstName} Profile</h2>
           <TextInput 
             inputId="firstName"
             fieldName="firstName"
