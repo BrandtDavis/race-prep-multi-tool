f59c880224620c87857079aec1b1b4e6cb57bc6f
Brandt Davis <brandtd77@gmail.com>
2025-04-19 18:42:40 -0600

    Add date input component

---
+++ client/src/components/inputs/DateInput.tsx
@@ -0,0 +1,41 @@
+type props = {
+    inputId: string;
+    fieldName: string;
+    fieldValue: string;
+    onChange: React.ChangeEventHandler;
+    labelValue: string;
+    placeholder: string;
+  };
+  
+  function DateInput({
+    inputId,
+    fieldName,
+    fieldValue,
+    onChange,
+    labelValue,
+    placeholder,
+  }: props) {
+    return (
+      <>
+        <label
+          className="flex text-gray-700 text-sm font-bold mb-2"
+          htmlFor={inputId}
+        >
+          {labelValue}
+        </label>
+  
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="date"
+          id={inputId}
+          name={fieldName}
+          onChange={onChange}
+          value={fieldValue}
+          placeholder={placeholder}
+        />
+      </>
+    );
+  }
+  
+  export default DateInput;
+  
