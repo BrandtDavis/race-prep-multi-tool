3aca4f4414b210b27a4c346142f5b303d06afdc5
Brandt Davis <brandtd77@gmail.com>
2025-05-04 15:05:57 -0600

    Add boilerplate for user model

---
+++ server/race_multi_tool/models/user.py
@@ -0,0 +1,11 @@
+""" Handle user registration, login, logout actions """
+
+# from uuid import uuid4
+
+class User:
+    """ Defines user data and related methods """
+    def insert_user(self):
+        """ Insert a new User to the DB """
+
+    def update_user(self):
+        """ Update a user's information """
