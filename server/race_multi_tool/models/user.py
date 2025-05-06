d5a07bb8add45e2fc399fd3a9bb5c30fc93a8784
Brandt Davis <brandtd77@gmail.com>
2025-05-05 22:27:06 -0600

    Add methods related to new user registration

--- server/race_multi_tool/models/user.py
+++ server/race_multi_tool/models/user.py
@@ -1,11 +1,53 @@
 """ Handle user registration, login, logout actions """
 
-# from uuid import uuid4
+from uuid import uuid4, UUID
+import psycopg2
+from ..db.db import DatabaseConnection
 
 class User:
     """ Defines user data and related methods """
-    def insert_user(self):
+    conn: psycopg2.extensions.connection
+    cur: psycopg2.extensions.cursor
+
+    id: UUID
+    first_name: str
+    last_name: str
+    email:str
+
+    def __init__(self, db: DatabaseConnection):
+        self.conn = db.get_connection()
+        self.cur = self.conn.cursor()
+
+    def get_uuid(self):
+        """ Return a uuid """
+        return uuid4().hex
+
+    def insert_user(self, email: str, password: str, first_name: str, last_name: str) -> str:
         """ Insert a new User to the DB """
+        self.cur.execute(f"\
+                 INSERT INTO users (id, email, password, first_name, last_name)\
+                 VALUES ('{self.get_uuid()}', '{email}', '{password}', '{first_name}', '{last_name}')\
+                 returning id;\
+                ")
+        new_row_id = self.cur.fetchone()
+        if new_row_id is not None:
+            self.conn.commit()
+            return "Success"
+
+        return "Error"
+
+    def email_exists(self, email: str) -> bool:
+        """ Determine if email is already in use """
+        self.cur.execute(f"SELECT * FROM users WHERE email = '{email}'")
+
+        result = self.cur.fetchone()
+        print(result)
+        return result is not None
+
+    def get_user_data(self, user_id: UUID):
+        """ Query for a user with a given id """
+        self.cur.execute(f"SELECT * FROM users WHERE id = '{user_id}'")
+        return self.cur.fetchone()
 
     def update_user(self):
         """ Update a user's information """
