fd63d20d9be9bc47c42c2b3e5bf8018733288ef0
Brandt Davis <brandtd77@gmail.com>
2025-05-11 23:23:10 -0600

    Move create_activity function

--- server/race_multi_tool/models/activity.py
+++ server/race_multi_tool/models/activity.py
@@ -1,6 +1,7 @@
 """ Handle CRUD actions for activities"""
 
 from uuid import uuid4, UUID
+from ..utils.utils import get_uuid
 import psycopg2
 from ..db.db import DatabaseConnection
 
@@ -21,8 +22,25 @@ class Activity:
         """ Return a uuid """
         return uuid4().hex
 
-    def create_activity(self, db, activity_id):
+    def create_activity(self, activity_data):
         """ Get an activity """
+
+        insert_query = """
+            INSERT into activities (id, activity_name, activity_date, activity_type, distance, units)
+            VALUES (uuid_generate_v4(), %s, %s, %s, %s, %s);
+        """
+
+        data = {"id": get_uuid()}
+
+        data = get_uuid(), \
+           activity_data["activity_name"], \
+           activity_data["activity_date"], \
+           activity_data["activity_type"], \
+           activity_data["distance"], \
+           activity_data["units"]
+
+        self.cur.execute(insert_query, data)
+        self.conn.commit()
 
     def get_activity(self, db, activity_id):
         """ Get an activity """
